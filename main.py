2ab2fa6d07b5de24df187cc95926b9556a59fb0d
derov81 <maniprutby@gmail.com>
2022-05-21 13:19:58 -0400

    First version , it is disaner

--- main.py
+++ main.py
@@ -4,15 +4,40 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import kivy
 from kivy.app import App
-from kivy.uix.button import  Button
+from kivy.uix.button import Button
+from kivy.base import runTouchApp
+from kivy.uix.spinner import Spinner
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from  kivy.uix.pagelayout import PageLayout
 
 
-class MyApp(App):
+
+class Container(FloatLayout):
+    pass
+
+class Pages(PageLayout):
+    pass
+
+
+
+class CalcCaloryApp(App):
     def build(self):
-        return Button(text='Hello World!!')
+        self.load_kv("CalcCalory.kv")
+        return Container()
+
+
+
+
+
+
+
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    MyApp().run()
+    CalcCaloryApp().run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
